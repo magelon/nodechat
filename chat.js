28a8bbf48c804c681cc26e2bcf63ed52f6c3c3f3
lon <alfardproject@gmail.com>
2016-02-26 15:18:48 -0800

    0.1
    reading data from connection

---
+++ chat.js
@@ -0,0 +1,23 @@
+﻿var net = require('net');
+
+var server = net.createServer();
+
+server.on('connection', function(socket){
+	console.log('got a new connection');
+	socket.on('data',function(data){
+		console.log('got data:',data);
+	});
+	
+});
+
+server.on('error',function(err){
+	console.log('server error:', err.message);
+
+});
+
+server.on('close',function(){
+	console.log('server closed');
+
+});
+
+server.listen(4001);
