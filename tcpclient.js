a5b9f91d0c3634f5813b1383543c708446e134f7
lon <alfardproject@gmail.com>
2016-02-26 17:15:55 -0800

    0.4
    send receive data from tcp server

---
+++ tcpclient.js
@@ -0,0 +1,25 @@
+﻿
+
+var net = require('net');
+
+var port = 4000;
+
+var host="www.acme.com";
+
+var conn= net.createConnection(port,host);
+
+function connectionListener(conn){
+	console.log('We have a new connection');
+}
+
+var conn = net.createConnection(port, connectionListener);
+
+conn.write('Here is a string for you!');
+
+var buffer = new Buffer('Hello World!');
+
+conn.write(buffer);
+
+conn.on('data',function(data){
+	console.log('some data has arrived:',data);
+});
--- webserver.js
+++ webserver.js
@@ -1,12 +1,6 @@
-﻿var http = require('http');
-
-var server = http.createServer();
-
-server.on('request',function(req,res){
-	res.writeHead(200,{'Content-Type':'text/plain'});
-	res.write('Hello world!');
-	res.end();
-
-});
-
-server.listen(4000);
+﻿
+var util =require('util');
+require('http').createServer(function(req, res) {
+ res.writeHead(200, {'Content-Type': 'text/plain'});
+ res.end(util.inspect(req.headers));
+}).listen(4000);
