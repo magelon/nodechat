c14101483779ac975d47e823c95d0782cbccddbb
lon <alfardproject@gmail.com>
2016-02-26 16:23:32 -0800

    0.4
    webserver

---
+++ webserver.js
@@ -0,0 +1,12 @@
+﻿var http = require('http');
+
+var server = http.createServer();
+
+server.on('request',function(req,res){
+	res.writeHead(200,{'Content-Type':'text/plain'});
+	res.write('Hello world!');
+	res.end();
+
+});
+
+server.listen(4000);
